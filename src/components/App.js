8450a6c7778d7f9ed873c7ae71372028378b3fff
Ashley Vick <ashleyvick@github.com>
2021-04-29 19:34:46 -0600

    I fixed a few things in POST. hope this works now.

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
-import Post from './Post/Post';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
+import Post from "./Post/Post";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-    axios.get('https://practiceapi.devmountain.com/api/posts')
-    .then(results => {
-      this.setState({posts: results.data});
-    });
+    axios
+      .get("https://practiceapi.devmountain.com/api/posts")
+      .then((results) => {
+        this.setState({ posts: results.data });
+      });
   }
 
   updatePost(id, text) {
-    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
-    .then(results => {
-      this.setState({posts: results.data});
-    })
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then((results) => {
+        this.setState({ posts: results.data });
+      });
   }
 
   deletePost(id) {
-    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
-    .then(results => {
-      this.setState({posts:results.data});
-    });
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then((results) => {
+        this.setState({ posts: results.data });
+      });
   }
 
   createPost(text) {
-    axios.post('https://practiceapi.devmountain.com/api/posts', {text})
-    .then(results => {
-      this.setState({posts:results.data});
-    });
+    axios
+      .post("https://practiceapi.devmountain.com/api/posts", { text })
+      .then((results) => {
+        this.setState({ posts: results.data });
+      });
   }
 
   render() {
@@ -55,19 +59,20 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
-          <Compose />
-          {
-            posts.map(post => (
-              <Post key={post.id} 
-              text={post.text} 
-              date={post.date}
-              updatePostsFn={this.updatePost}
-              id={post.id}
-              deletePostFn={this.deletePost}
-              createPostFn={this.createPost} />
-            ))
-          }
+          <Compose createPostFn={this.createPost} />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                text={post.text}
+                date={post.date}
+                updatePostsFn={this.updatePost}
+                id={post.id}
+                deletePostFn={this.deletePost}
+                updatePostFn={this.updatePost}
+              />
+            );
+          })}
         </section>
       </div>
     );
